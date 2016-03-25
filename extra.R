80e87398c8791a3de820e98b574a2846f46b6d6d
iclearn <iclearn@users.noreply.github.com>
2016-03-25 17:31:15 +0530

    updated extra.R

--- extra.R
+++ extra.R
@@ -1,12 +1,18 @@
+## supervised topic modelling using topicmodels package
 library(topicmodels)
+## using data AssociatedPress in topicmodels
 data("AssociatedPress")
 
+## the training subset
 train <- AssociatedPress[1:100, ]
+## the testing subset
 test <- AssociatedPress[101:150, ]
 
+## traiing using LDA algorithm
 train.lda <- LDA(train,5)
+## stroring the topics in train.topics
 (train.topics <- topics(train.lda))
 
-
+## test.topics include topics as discovered using the training set
 test.topics <- posterior(train.lda,test)
-(test.topics <- apply((test.topics)$topics, 1, which.max))
+
