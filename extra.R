40a0fed03b9515088074d86834a10561e7b1c9bf
iclearn <iclearn@users.noreply.github.com>
2016-03-25 17:17:48 +0530

    added extra task

---
+++ extra.R
@@ -0,0 +1,12 @@
+library(topicmodels)
+data("AssociatedPress")
+
+train <- AssociatedPress[1:100, ]
+test <- AssociatedPress[101:150, ]
+
+train.lda <- LDA(train,5)
+(train.topics <- topics(train.lda))
+
+
+test.topics <- posterior(train.lda,test)
+(test.topics <- apply((test.topics)$topics, 1, which.max))
